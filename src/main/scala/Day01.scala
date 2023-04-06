bd354f8bf5dc4362dd4e8c6f780e9f2ee1916742
Ruben van Schaik <rubenvanschaik@gmail.com>
2023-04-06 16:59:23 +0200

    initial load

---
+++ src/main/scala/Day01.scala
@@ -0,0 +1,19 @@
+import scala.io._
+
+object Day01 extends App:
+
+  val day: String =
+    this.getClass.getName.drop(3).init
+
+  val start1: Long =
+    System.currentTimeMillis
+
+  val frequencies: List[Int] =
+    Source
+      .fromResource(s"input$day.txt")
+      .getLines
+      .map(s => s.toInt)
+      .toList
+
+  val answer1: Int = frequencies.sum
+  println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
---
+++ src/test/scala/TestSolutions.scala
@@ -0,0 +1,7 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class TestSolutions extends AnyFunSuite:
+
+  test("Day01 [26ms]") {
+    assertResult(477)(actual = Day01.answer1)
+  }
