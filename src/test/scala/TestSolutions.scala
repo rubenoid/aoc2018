f65658f88a1dd9d5bf8329a21956429bde4d9c27
Ruben van Schaik <rubenvanschaik@gmail.com>
2023-04-12 14:05:33 +0200

    added tests

--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -2,6 +2,21 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestSolutions extends AnyFunSuite:
 
-  test("Day01 [26ms]") {
-    assertResult(477)(actual = Day01.answer1)
+  test("Day01 Part 1") {
+    assertResult(547)(actual = Day01.answer1)
+  }
+  test("Day01 Part 2") {
+    assertResult(76414)(actual = Day01.answer2)
+  }
+  test("Day02 Part 1") {
+    assertResult(5928)(actual = Day02.answer1)
+  }
+  test("Day02 Part 2") {
+    assertResult("bqlporuexkwzyabnmgjqctvfs")(actual = Day02.answer2)
+  }
+  test("Day03 Part 1") {
+    assertResult(124850)(actual = Day03.answer1)
+  }
+  test("Day03 Part 2") {
+    assertResult(1097)(actual = Day03.answer2)
   }
