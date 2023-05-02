3f44738dfdfe574be2a57aa0e578bdee8544eeb5
Ruben van Schaik <rubenvanschaik@gmail.com>
2023-05-02 13:17:06 +0200

    init

---
+++ src/main/scala/Day05.scala
@@ -0,0 +1,28 @@
+import scala.annotation.tailrec
+import scala.io.*
+
+object Day05 extends App:
+
+  val day: String =
+    this.getClass.getName.drop(3).init
+
+  val start1: Long =
+    System.currentTimeMillis
+
+  val frequencies: List[Int] =
+    Source
+      .fromResource(s"input$day.txt")
+      .getLines
+      .map(s => s.toInt)
+      .toList
+
+  val answer1: Int = 999
+  println(Console.BLUE + s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+
+  /* Part 2 */
+  val start2: Long =
+    System.currentTimeMillis
+  
+  val answer2: Int = 999
+
+  println(Console.BLUE + s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -26,3 +26,9 @@ class TestSolutions extends AnyFunSuite:
   test("Day04 Part 2") {
     assertResult(136571)(actual = Day04.answer2)
   }
+  test("Day05 Part 1") {
+    assertResult(999)(actual = Day05.answer1)
+  }
+  test("Day05 Part 2") {
+    assertResult(999)(actual = Day05.answer2)
+  }
