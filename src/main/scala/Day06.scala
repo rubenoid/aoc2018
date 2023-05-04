4cdd1f5ee2f0f273289a86f5aa2bda44ead6c32f
Ruben van Schaik <rubenvanschaik@gmail.com>
2023-05-04 14:40:00 +0200

    part 1 & 2

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -1,3 +1,4 @@
+import scala.+:
 import scala.annotation.tailrec
 import scala.io.*
 
@@ -9,22 +10,108 @@ object Day06 extends App:
   val start1: Long =
     System.currentTimeMillis
 
-  val coordinates: List[Int] =
+  val input =
     Source
       .fromResource(s"input$day.txt")
       .getLines
-      .map(s => s.toInt)
-      .toList
 
+  val pattern = """(\d+), (\d+)""".r
+
+  val coordinates = {
+    var tmp: List[(Int, Int)] = List[(Int, Int)]()
+    for (l <- input) {
+      l match
+        case pattern(x, y) =>
+          val t = (y.toInt, x.toInt)
+          tmp = tmp :+ t
+    }
+    tmp
+  }
   println(coordinates)
 
-  val answer1: Int = 0
+  val dim = 400
+  var field = Array.fill[Int](dim,dim)(-2)
+
+  def manhattenDist(x1: Int, y1: Int, x2: Int, y2: Int): Int = {
+    math.abs(x1 - x2) + math.abs(y1 - y2)
+  }
+
+  /*
+  Returns the index of the closest point out of the list points
+  -1 if there are more than one point that is closest
+  */
+  def indexToClosestPoint(x1: Int, y1: Int, points: List[(Int, Int)]): Int = {
+    val distances = for {
+      point <- points
+      dist = manhattenDist(x1, y1, point._1, point._2)
+    } yield dist
+    val minimum = distances.min
+    val amountOfPoints = distances.count(x => x == minimum)
+    if (amountOfPoints > 1)
+      -1
+    else
+      distances.indexOf(distances.min)
+  }
+
+  def pointIsInList(x1: Int, y1: Int, points: List[(Int, Int)]): Boolean = {
+    points.contains((x1, y1))
+  }
+
+  for (i <- field.indices)
+    for (j <- field(0).indices)
+//      if (!pointIsInList(i, j, coordinates))
+        val indexPoint = indexToClosestPoint(i, j, coordinates)
+        if (indexPoint == -1)
+          field(i)(j) = -1
+        else
+          field(i)(j) = indexPoint
+
+  def findMaxPoints(): Int = {
+    var keys = for {
+      i <- 1 until field.length
+      j <- 1 until field.length
+      key = field(i)(j)
+    } yield(key)
+
+    for (i <- field.indices)
+      val l = field.length
+      keys = keys.filter(x => {
+        x != field(i)(0) &&
+          x != field(i)(l - 1) &&
+          x != field(0)(i) &&
+          x != field(l - 1)(i)
+      })
+    keys.filter(_ >= 0)
+      .groupBy(identity)
+      .map(_._2.length)
+      .max
+
+  }
+  println(findMaxPoints())
+
+  val answer1: Int = findMaxPoints()
   println(Console.BLUE + s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
   /* Part 2 */
   val start2: Long =
     System.currentTimeMillis
 
-  val answer2: Int = 0
+  def distanceToPoints(x1: Int, y1: Int, points: List[(Int, Int)]): Int =
+    val distances = for {
+      point <- points
+      dist = manhattenDist(x1, y1, point._1, point._2)
+    } yield dist
+    distances.sum
+
+  val amountRegionPoints = {
+    val distancePoints = for {
+      i <- 0 until dim
+      j <- 0 until dim
+      dist = distanceToPoints(i, j, coordinates)
+    } yield dist
+    distancePoints.count(_ < 10000)
+  }
+
+  val answer2: Int = amountRegionPoints
 
   println(Console.BLUE + s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -1,40 +1,40 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class TestSolutions extends AnyFunSuite:
-
-  test("Day01 Part 1") {
-    assertResult(547)(actual = Day01.answer1)
-  }
-  test("Day01 Part 2") {
-    assertResult(76414)(actual = Day01.answer2)
-  }
-  test("Day02 Part 1") {
-    assertResult(5928)(actual = Day02.answer1)
-  }
-  test("Day02 Part 2") {
-    assertResult("bqlporuexkwzyabnmgjqctvfs")(actual = Day02.answer2)
-  }
-  test("Day03 Part 1") {
-    assertResult(124850)(actual = Day03.answer1)
-  }
-  test("Day03 Part 2") {
-    assertResult(1097)(actual = Day03.answer2)
-  }
-  test("Day04 Part 1") {
-    assertResult(30630)(actual = Day04.answer1)
-  }
-  test("Day04 Part 2") {
-    assertResult(136571)(actual = Day04.answer2)
-  }
-  test("Day05 Part 1") {
-    assertResult(10774)(actual = Day05.answer1)
-  }
-  test("Day05 Part 2") {
-    assertResult(5122)(actual = Day05.answer2)
-  }
+//
+//  test("Day01 Part 1") {
+//    assertResult(547)(actual = Day01.answer1)
+//  }
+//  test("Day01 Part 2") {
+//    assertResult(76414)(actual = Day01.answer2)
+//  }
+//  test("Day02 Part 1") {
+//    assertResult(5928)(actual = Day02.answer1)
+//  }
+//  test("Day02 Part 2") {
+//    assertResult("bqlporuexkwzyabnmgjqctvfs")(actual = Day02.answer2)
+//  }
+//  test("Day03 Part 1") {
+//    assertResult(124850)(actual = Day03.answer1)
+//  }
+//  test("Day03 Part 2") {
+//    assertResult(1097)(actual = Day03.answer2)
+//  }
+//  test("Day04 Part 1") {
+//    assertResult(30630)(actual = Day04.answer1)
+//  }
+//  test("Day04 Part 2") {
+//    assertResult(136571)(actual = Day04.answer2)
+//  }
+//  test("Day05 Part 1") {
+//    assertResult(10774)(actual = Day05.answer1)
+//  }
+//  test("Day05 Part 2") {
+//    assertResult(5122)(actual = Day05.answer2)
+//  }
   test("Day06 Part 1") {
-    assertResult(999)(actual = Day06.answer1)
+    assertResult(5429)(actual = Day06.answer1)
   }
   test("Day06 Part 2") {
-    assertResult(999)(actual = Day06.answer2)
+    assertResult(32614)(actual = Day06.answer2)
   }
